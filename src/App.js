def6817a462d8b562e2855d98078ad2e57a4bcad
Victor Maciel <63254205+victorgmaciel@users.noreply.github.com>
2021-05-27 15:13:06 -0700

    added work on appjs.

--- src/App.js
+++ src/App.js
@@ -20,10 +20,10 @@ function App() {
           <input type="text" id="searchQuery" placeholder="Search..." />
           <button id="searchButton"><i className="fas fa-search fa-flip-horizontal" aria-label="search"></i></button>
         </div>
-          <div className="container">
-              <div className="row outerMostRow">
+        <div className="container">
+          <div className="row outerMostRow">
           </div>
-          </div>
+        </div>
       </main>
 
 
@@ -31,7 +31,7 @@ function App() {
 
 
 
-    {/* index page footer*/}
+      {/* index page footer*/}
       <footer className="footer">
         <p>2021 &#169;</p>
       </footer>
@@ -44,18 +44,18 @@ function NavBar() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark navbar-custom">
-          <a className="navbar-brand" id="nav-elem" href="#">GameFinder</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-item nav-link text-light" href="#">Home</a> {/*to fix!*/}
-              <a className="nav-item nav-link text-light" href="">Landing</a>{/*to fix!*/}
-              <a className="nav-item nav-link text-light" href="">About</a> {/*to fix!*/}
-            </div>
-          </div>
-        </nav>
+      <a className="navbar-brand" id="nav-elem" href="#">GameFinder</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link text-light" href="#">Home</a> {/*to fix!*/}
+          <a className="nav-item nav-link text-light" href="">Landing</a>{/*to fix!*/}
+          <a className="nav-item nav-link text-light" href="">About</a> {/*to fix!*/}
+        </div>
+      </div>
+    </nav>
 
   )
 }

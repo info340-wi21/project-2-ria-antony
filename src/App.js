a8b116769a6d985e085f75d0a43d744a37b19876
Victor Maciel <63254205+victorgmaciel@users.noreply.github.com>
2021-05-27 14:31:51 -0700

    Added NavBar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 
+import React from 'react'; //react library 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; //bootstrap
+
+
 
 function App() {
   return (
     <body>
+
+      {/*header*/}
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-dark navbar-custom">
+          <a className="navbar-brand" id="nav-elem" href="#">GameFinder</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link text-light" href="#">Home</a> {/*to fix!*/}
+              <a className="nav-item nav-link text-light" href="">Landing</a>{/*to fix!*/}
+              <a className="nav-item nav-link text-light" href="">About</a> {/*to fix!*/}
+            </div>
+          </div>
+        </nav>
+      </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <footer className="footer">
+        <p>2021 &#169;</p>
+      </footer>
+
+
     </body>
   );
 }

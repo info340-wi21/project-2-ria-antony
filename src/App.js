3b8f30a44be55e2096c58f88cd7bf658f67dbe54
Victor Maciel <63254205+victorgmaciel@users.noreply.github.com>
2021-05-29 12:53:42 -0700

    added for the cards.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import React from 'react'; //react library 
 import './App.css';
-import {AboutPage} from './About';
+import { AboutPage } from './About';
 import 'bootstrap/dist/css/bootstrap.min.css'; //bootstrap
 import { Button } from 'reactstrap';
-import {Route, Switch, Link, Redirect, NavLink} from 'react-router-dom';
+import { Route, Switch, Link, Redirect, NavLink } from 'react-router-dom';
 
 
 import games from './data/gameData.json';
@@ -25,20 +25,20 @@ function App(props) {
 
       {/* index page searchBox*/}
       <main>
-      <Switch>
-        <Route exact path="/"> <Search /> 
-        <div className="container">
-        <div className="row">
-          <RenderCard gameData={gameData[0]}/>
-        </div>
-      </div>
-      </Route>
-        <Route path="/about"> <AboutPage /> </Route>
-        <Redirect to="/" />
-      </Switch>
+        <Switch>
+          <Route exact path="/"> <Search />
+            <div className="container">
+              <div className="row">
+                <RenderCard gameData={gameData[0]} />
+              </div>
+            </div>
+          </Route>
+          <Route path="/about"> <AboutPage /> </Route>
+          <Redirect to="/" />
+        </Switch>
 
 
-            
+
       </main>
 
       {/* index page footer*/}
@@ -60,7 +60,7 @@ function NavBar() {
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-        
+
           <NavLink className="nav-item nav-link text-light" exact to="/">Home</NavLink>
           <NavLink className="nav-item nav-link text-light" to="/about">About</NavLink>
         </div>
@@ -70,14 +70,14 @@ function NavBar() {
   )
 }
 
-function Search(){
+function Search() {
   return (
     <div>
       <div className="searchBox" role="search">
-          <input type="text" id="searchQuery" placeholder="Search..." />
-          <Button id="searchButton"><i className="fas fa-search fa-flip-horizontal" aria-label="search"></i>Search</Button>
+        <input type="text" id="searchQuery" placeholder="Search..." />
+        <Button id="searchButton"><i className="fas fa-search fa-flip-horizontal" aria-label="search"></i>Search</Button>
       </div>
-  </div>
+    </div>
   );
 }
 
@@ -114,60 +114,65 @@ function RenderCardList(props) {
   return (
     <div className="container">
       <div className="row">
-            {gameList}
+        {gameList}
       </div>
     </div>
   );
 }
 
 function RenderCard(props) {
- 
-//  div1.addEventListener("click", function(){
-//   if (clicked == false){
-//     div6.innerHTML = "";
-    
-//     let genreElem = document.createElement("p") 
-//     genreElem.textContent = "Genre:" + " " + game.Genre;
-//     div6.appendChild(genreElem);
 
-//     let releaseElem = document.createElement("p") 
-//     releaseElem.textContent = "Release Date: " + " " + game.Release_Date; 
-//     div6.appendChild(releaseElem);
+  //  div1.addEventListener("click", function(){
+  //   if (clicked == false){
+  //     div6.innerHTML = "";
 
-//     let communityLinkElem = document.createElement("a");
-//     communityLinkElem.textContent = "Reddit Community"
-//     communityLinkElem.href = game.Subreddit;
-//     div6.appendChild(communityLinkElem);
-//     clicked = true;
-//   }
-//   else{
-//     div6.innerHTML = "";
-//     div6.appendChild(header2Elem);
-//     clicked = false;
-//   }
-//  });
+  //     let genreElem = document.createElement("p") 
+  //     genreElem.textContent = "Genre:" + " " + game.Genre;
+  //     div6.appendChild(genreElem);
+
+  //     let releaseElem = document.createElement("p") 
+  //     releaseElem.textContent = "Release Date: " + " " + game.Release_Date; 
+  //     div6.appendChild(releaseElem);
+
+  //     let communityLinkElem = document.createElement("a");
+  //     communityLinkElem.textContent = "Reddit Community"
+  //     communityLinkElem.href = game.Subreddit;
+  //     div6.appendChild(communityLinkElem);
+  //     clicked = true;
+  //   }
+  //   else{
+  //     div6.innerHTML = "";
+  //     div6.appendChild(header2Elem);
+  //     clicked = false;
+  //   }
+  //  });
   // let gameArray = props.
 
   console.log(props.gameData.Game)
-      return (
-        <div className="col-12 col-md-6 col-lg-6 col-xl-3 d-flex">
-          <div className="card mb-4 bg-secondary">
-            <div className="card-body">
-              <div className="row">
-                <div className="col col-sm col-xl-12">
-                  <img className="pb-3" src={props.gameData.Picture} alt={props.gameData.Game} />
-                </div>
-                <div className="cols-sm">
-                  <h2 className="card-title">{props.gameData.Game}</h2>
-                </div>
-              </div>
+  return (
+    <div className="col-12 col-md-6 col-lg-6 col-xl-3 d-flex">
+      <div className="card mb-4 bg-secondary">
+        <div className="card-body">
+          <div className="row">
+            <div className="col col-sm col-xl-12">
+              <img className="card-img-top" src={props.gameData.Picture} alt={props.gameData.Game} />
+            </div>
+            <div className="cols-sm">
+              <h2 className="card-title">{props.gameData.Game}</h2>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">{props.gameData.Release_Date}</li>
+                <li class="list-group-item">{props.gameData.Subreddit}</li>
+                <li class="list-group-item">{props.gameData.Discord}</li>
+              </ul>
             </div>
           </div>
         </div>
-      );
-  
-      // event listener for second data view
-  
-     
-  }
+      </div>
+    </div>
+  );
+
+  // event listener for second data view
+
+
+}
 export default App;

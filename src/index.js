9b572e90451cac62da8ab862e28178d1aaf7d32b
Ria Antony <rantony@uw.edu>
2021-05-30 19:24:53 -0700

    draft ready

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+//for firebase
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';

c73687f07d8cd9c68f82cecebb179c82e4904595
Ria Antony <rantony@uw.edu>
2021-06-04 16:00:50 -0700

    Bugs fixed, thanks to Lauren

--- src/FavPage.js
+++ src/FavPage.js
@@ -1,89 +1,97 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react'; 
+import firebase from 'firebase';
 
-let allFavsArray = []; //empty array that will hold the array version of firebase data
 
 function FavPage(props) {
-    console.log(props.rootRef);
     return (
         <div>
             <h1 id="aboutHeader">Favorites</h1>
-                <FavList rootRef={props.rootRef}></FavList>
+                <FavList ></FavList>
         </div>
     );
   }
 
-  function FavList(props){
-    let reference = props.rootRef;
-    console.log(reference);
-    reference.on('value', snapshot => { //Kinda complicated but bascially turns firebase data into array form so it can be mapped
-      let favShot = snapshot.val();
-      if (favShot !== null){
-        const allFavsObject = favShot;
-        const allFavsKeys = Object.keys(allFavsObject);
-          allFavsArray = allFavsKeys.map((key) => { 
-          const singleFavCopy = {...allFavsObject[key]}; 
-          singleFavCopy.key = key;
-          return singleFavCopy;
-        });
-      }
-      else{
-        allFavsArray = [];
-      }
-    });
-    let favList = allFavsArray.map((gameObj) => { //firebase data in array form being mapped
-        return <RenderFavCard key={gameObj.Game} gameData={gameObj} rootRef={reference}/>
+  function FavList(){
+    // creating a state variable with the list of favorited game objects 
+    const [favoriteArray, setFavoriteArray] = useState([]);
+    
+    // When this function is first ran, it will fetchData.
+    // we need to fetch data in a function otherwise it'll re-render to infinity 
+    useEffect(() => {
+      fetchData()
+    }, [])
+    // fetching the data from firebase and updating state accordingly
+    const fetchData = () => {  
+      let allFavsArray = []; //empty array that will hold the array version of firebase data
+      console.log("reloading the data")
+      firebase.database().ref().on('value', snapshot => { //Kinda complicated but bascially turns firebase data into array form so it can be mapped
+        let favShot = snapshot.val();
+        if (favShot !== null){
+          const allFavsObject = favShot;
+          const allFavsKeys = Object.keys(allFavsObject);
+            allFavsArray = allFavsKeys.map((key) => { 
+              const singleFavCopy = {...allFavsObject[key]}; 
+              singleFavCopy.key = key;
+              return singleFavCopy;
+            });
+          setFavoriteArray(allFavsArray)
+        }
+        else{
+          setFavoriteArray([])
+        }
       });
-    return(
-        <div className="container">
-            <div className="row">
-                {favList}
-            </div>
-        </div>
-    );
-  }
-
-  function RenderFavCard(props){
-    let rem = "Remove";
-    let key = "";
-    const [value, setValue] = useState(0); // integer state
-    const ForceUpdate = () =>{
-      return () => setValue(value => this.value + 1); // update the state to force render
     }
-    const handleClick = (event) => {
-      for (let i = 0; i < allFavsArray.length; i++) { //Looks through firebase "array" to find the key of the gameObject that's "remove" button got pushed
-        if (allFavsArray[i] === props.gameData){
-          key = allFavsArray[i].key;
+    
+    // when the "remove" button is clicked, will remove object from firebase and call fetchData to update the list view 
+    // props are event and the game object
+    const handleClick = (event, data) => {
+      let key = ""
+      for (let i = 0; i < favoriteArray.length; i++) { //Looks through firebase "array" to find the key of the gameObject that's "remove" button got pushed
+        console.log(favoriteArray[i])
+        if (favoriteArray[i] === data){
+          console.log("found it!")
+          key = favoriteArray[i].key;
         }
       }
       if (key !== ""){
-        props.rootRef.child(key).remove(); //Removes gameObject from Firebase data based on key
+        console.log("removing the key")
+        firebase.database().ref().child(key).remove(); //Removes gameObject from Firebase data based on key
       }
-      console.log(props);
-      return <FavList rootRef={props.rootRef}></FavList>
+      
+      // update the data
+      fetchData();
+      event.preventDefault()
     }
   
-    return (
-        <div className="col-12 col-md-6 col-lg-6 col-xl-3 d-flex">
-          <div className="card mb-4 bg-secondary">
-            <div className="card-body">
-              <div className="row">
-                <div className="col col-sm col-xl-12">
-                  <img className="card-img-top" src={props.gameData.Picture} alt={props.gameData.Game} />
-                </div>
-                <div className="cols-sm">
-                  <h2 className="card-title">{props.gameData.Game}</h2>
-                  <p> Release Date: {props.gameData.Release_Date}</p>
-                  <p>Genre: {props.gameData.Genre}</p>
-                  <a className="text-white" target="_blank" rel="noopener noreferrer" href={props.gameData.Subreddit}>Reddit</a><br></br><br></br>
-                  <a className="text-white" target="_blank" rel="noopener noreferrer" href={props.gameData.Discord}>Discord</a><br></br><br></br>
-                  <button onClick={handleClick} className="re-button bg-danger text-white">{rem}</button>
-                </div>
-              </div>
+    return(
+        <div className="container">
+            <div className="row">
+                {favoriteArray.map((gameObj) => { //firebase data in array form being mapped
+                  return (
+                    <div className="col-12 col-md-6 col-lg-6 col-xl-3 d-flex">
+                      <div className="card mb-4 bg-secondary">
+                        <div className="card-body">
+                          <div className="row">
+                            <div className="col col-sm col-xl-12">
+                              <img className="card-img-top" src={gameObj.Picture} alt={gameObj.Game} />
+                            </div>
+                            <div className="cols-sm">
+                              <h2 className="card-title">{gameObj.Game}</h2>
+                              <p> Release Date: {gameObj.Release_Date}</p>
+                              <p>Genre: {gameObj.Genre}</p>
+                              <a className="text-white" target="_blank" rel="noopener noreferrer" href={gameObj.Subreddit}>Reddit</a><br></br><br></br>
+                              <a className="text-white" target="_blank" rel="noopener noreferrer" href={gameObj.Discord}>Discord</a><br></br><br></br>
+                              <button onClick={(event) => handleClick(event, gameObj)} className="re-button bg-danger text-white">remove</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    )
+                })}
             </div>
-          </div>
         </div>
     );
   }
-  export default FavPage;
 
-  export {FavPage};
+export default FavPage;

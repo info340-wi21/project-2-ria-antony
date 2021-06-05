d5c8cac425c2007144dd5c59debb0e87553bcb23
Ria Antony <rantony@uw.edu>
2021-06-04 17:15:44 -0700

    Undid some changes cuz they were being buggy

--- src/RenderCardList.js
+++ src/RenderCardList.js
@@ -5,21 +5,21 @@ function RenderCardList(props) {
 
     let searchTokens = props.searchTerm.toLowerCase().split(" ");
     
-    let botwArray = ["breath", "wild"];
-    let ootArray = ["ocarina", "time"];
-    let mmArray = ["majora's","majora", "mask"];
-    let laArray = ["link's", "link", "awakening"];
+    let botwArray = ["breath", "of", "the", "wild"];
+    let ootArray = ["ocarina", "of", "time"];
+    let mmArray = ["majora's", "mask"];
+    let laArray = ["link's", "awakening"];
     let tpArray = ["twilight", "princess"];//Zelda
     let forArray = ["forza", "horizon", "4"];
     let crewArray = ["the", "crew", "2"];
-    let nfsArray = ["need", "speed", "heat"];
+    let nfsArray = ["need", "for", "speed", "heat"];
     let dirtArray = ["dirt", "5"];
     let fArray = ["f1", "2020"];//racing
-    let warArray = ["call", "duty:", "warzone"];
+    let warArray = ["call", "of", "duty:", "warzone"];
     let bfArray = ["battlefield", "5"];
     let destArray = ["destiny", "2"];
     let valArray = ["valorant"];
-    let csgoArray = ["counter-", "counter","strike", "global", "offensive"];//shooter
+    let csgoArray = ["counter-", "strike", "global", "offensive"];//shooter
   
     let zelda = [botwArray, ootArray, mmArray, laArray, tpArray];
     let racing = [forArray, crewArray, nfsArray, dirtArray, fArray];
